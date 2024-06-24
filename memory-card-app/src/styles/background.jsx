77548c67d2f7ad1c259fec558cddedae001c9c0e
Rustum <rustumarellano@gmail.com>
2024-06-24 14:08:33 +0800

    uninstalled react-sound, and installed use-sound to resolve the legacy-peer conflict

--- memory-card-app/src/styles/background.jsx
+++ memory-card-app/src/styles/background.jsx
@@ -1,12 +1,17 @@
-import Sound from 'react-sound';
-import bg from './bg/haha.mp3'
+import { useEffect } from 'react';
+import useSound from 'use-sound';
+import bg from './bg/haha.mp3';
 
-export default function BackgroundMusic({isPlaying}){
-    return(
-        <Sound
-            url={bg}
-            playStatus={isPlaying? Sound.status.PLAYING : Sound.status.PAUSED}
-            loop={true}
-        />
-    )
+export default function BackgroundMusic({ isPlaying }) {
+  const [play, { stop }] = useSound(bg, { loop: true });
+
+  useEffect(() => {
+    if (isPlaying) {
+      play();
+    } else {
+      stop();
+    }
+  }, [isPlaying, play, stop]);
+
+  return null;
 }
